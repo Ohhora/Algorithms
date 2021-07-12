d472155934e0b1e35b054f369ee4e864acf13ea7
Hugh <46857352+Ohhora@users.noreply.github.com>
2021-07-12 22:31:02 +0900

    Update insert_sort_python.py

--- insert_sort_python.py
+++ insert_sort_python.py
@@ -1,1 +1,10 @@
+## source : https://www.daleseo.com/sort-insertion/
 
+
+def insert_sort(arr):
+    for i in range(1,len(arr)):
+        for j in range(i,0,-1):
+            if arr[j-1] >= arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+    return arr
+            
